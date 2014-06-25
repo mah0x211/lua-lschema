19de271f71eb6539002c3b73d959e1976d957d6b
Masatoshi Teruya <mah0x211@gmail.com>
2014-06-25 15:19:20 +0900

    add: errno.lua has been added.

---
+++ lib/ddl/errno.lua
@@ -0,0 +1,39 @@
+--[[
+  
+  Copyright (C) 2014 Masatoshi Teruya
+ 
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+ 
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+ 
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  
+  
+  lib/ddl/errno.lua
+  lua-lschema
+  Created by Masatoshi Teruya on 14/06/25.
+
+--]]
+
+return {
+    ENULL       = 1,
+    EDEFAULT    = 2,
+    ETYPE       = 3,
+    EMIN        = 4,
+    EMAX        = 5,
+    EPAT        = 6,
+    EENUM       = 7
+};
+
