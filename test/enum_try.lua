cb16c5b0e642328d8299e4bda60ff60ac3c67c45
Masatoshi Teruya <mah0x211@gmail.com>
2014-12-25 14:09:27 +0900

    add: test/enum_try.lua has been added.

---
+++ test/enum_try.lua
@@ -0,0 +1,70 @@
+local lschema = require('lschema');
+local myschema = ifNil( lschema.new('myschema') );
+local enum = myschema.enum;
+
+-- indentity pattern: ^[_a-zA-Z][_a-zA-Z0-9]*$
+-- invalid ident
+ifTrue(isolate(function()
+    enum '' {}
+end));
+ifTrue(isolate(function()
+    enum 'my-enum' {}
+end));
+ifTrue(isolate(function()
+    enum 'my.enum' {}
+end));
+
+-- test enum field ident
+-- invalid ident
+ifTrue(isolate(function()
+    enum 'myenum' {
+        1
+    }
+end));
+ifTrue(isolate(function()
+    enum 'myenum' {
+        'name-1'
+    }
+end));
+ifTrue(isolate(function()
+    enum 'myenum' {
+        'name.1'
+    }
+end));
+-- valid ident
+ifNotTrue(isolate(function()
+    enum 'myenum' {
+        'name1',
+        'name2'
+    }
+end));
+
+-- verify definition
+ifNil( enum.myenum );
+ifNotEqual( 1, enum.myenum.name1 );
+ifNotEqual( 2, enum.myenum.name2 );
+-- invalid duplicate enum ident
+ifTrue(isolate(function()
+    enum 'myenum' {}
+end));
+
+
+-- verify behavior
+ifNotEqual( enum.myenum('name1'), 'name1' );
+ifNotEqual( enum.myenum('name2'), 'name2' );
+-- verify error
+local res, err = enum.myenum('name3');
+local cmp = { 
+    errno = 6,
+    etype = "EENUM",
+    attr = { 
+        name1 = 1,
+        name2 = 2
+    }
+};
+ifNotNil( res );
+-- err must be table
+ifNotEqual( inspect(err), inspect(cmp) );
+
+
+
