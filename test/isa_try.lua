2b310244c575dfe383708bc038ecafcac23aa9b8
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-02 14:04:25 +0900

    add: test/isa_try.lua has been added.

---
+++ test/isa_try.lua
@@ -0,0 +1,35 @@
+local lschema = require('lschema');
+local schema = lschema.new('test');
+local isa = schema.isa;
+
+-- isa type
+for _, typ in ipairs({
+    'string',
+    'number',
+    'unsigned',
+    'int',
+    'int8',
+    'int16',
+    'int32',
+    'uint8',
+    'uint16',
+    'uint32',
+    'boolean',
+    'table',
+    'enum',
+    'struct'
+}) do
+    ifNotTrue(isolate(function()
+        isa( typ );
+    end));
+    -- does not support table array
+    if typ == 'table' then
+        ifTrue(isolate(function()
+            isa( typ .. '[]' );
+        end));
+    else
+        ifNotTrue(isolate(function()
+            isa( typ .. '[]' );
+        end));
+    end
+end
