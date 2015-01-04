b55c55443f949f325523165ea0ad0aa5e3f2a380
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-04 23:25:40 +0900

    add: test/isa_int_try.lua has been added.

---
+++ test/isa_int_try.lua
@@ -0,0 +1,61 @@
+local errno = require('lschema.ddl.errno');
+local lschema = require('lschema');
+local myschema = lschema.new('myschema');
+local isa = myschema.isa('int');
+local err, _;
+
+isa:makeCheck();
+-- invalid
+for _, val in ipairs({
+    -- boolean
+        true, false,
+    -- string
+        'string', '',
+    -- function
+        function()end,
+    -- table
+        {},
+    -- thread
+        coroutine.create(function() end),
+    -- inf
+        -(1/0), 1/0,
+    -- nan
+        0/0,
+    -- float
+        0.1,
+    -- negative float
+        -0.1
+}) do
+    _, err = isa( val );
+    ifNil( err );
+    ifNotEqual( err.errno, errno.ETYPE );
+    ifNotEqual( err.etype, 'ETYPE' );
+end
+
+-- valid
+for _, val in pairs({
+    -- number
+        0, 1, 
+    -- negative number
+        -0, -1,
+    -- float
+        0.0
+    -- negative float
+        -0.0,
+    -- int8
+        -128, 127,
+    -- int16
+        -32768, 32767,
+    -- int32
+        -2147483648, 2147483647,
+    -- uint8
+        255,
+    -- uint16
+        65535,
+    -- uint32
+        4294967295
+}) do
+    _, err = isa( val );
+    ifNotNil( err );
+    ifNotEqual( _, val );
+end
