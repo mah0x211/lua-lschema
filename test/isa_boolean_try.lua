11b983e578f65a605da985bdb1e5c0c4ffffd46f
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-04 23:55:47 +0900

    add: test/isa_boolean_try.lua has been added.

---
+++ test/isa_boolean_try.lua
@@ -0,0 +1,57 @@
+local errno = require('lschema.ddl.errno');
+local lschema = require('lschema');
+local myschema = lschema.new('myschema');
+local isa = myschema.isa('boolean');
+local err, _;
+
+isa:makeCheck();
+-- invalid
+for _, val in ipairs({
+    -- string
+        'string', '',
+    -- function
+        function()end,
+    -- table
+        {},
+    -- thread
+        coroutine.create(function() end),
+    -- inf
+        -(1/0), 1/0,
+    -- nan
+        0/0,
+    -- number
+        0, 1, 
+    -- negative number
+        -0, -1,
+    -- float
+        0.0, 0.1,
+    -- negative float
+        -0.0, -0.1,
+    -- int8
+        -128, 127,
+    -- int16
+        -32768, 32767,
+    -- int32
+        -2147483648, 2147483647,
+    -- uint8
+        255,
+    -- uint16
+        65535,
+    -- uint32
+        4294967295
+}) do
+    _, err = isa( val );
+    ifNil( err );
+    ifNotEqual( err.errno, errno.ETYPE );
+    ifNotEqual( err.etype, 'ETYPE' );
+end
+
+-- valid
+for _, val in pairs({
+    -- boolean
+        true, false
+}) do
+    _, err = isa( val );
+    ifNotNil( err );
+    ifNotEqual( _, val );
+end
