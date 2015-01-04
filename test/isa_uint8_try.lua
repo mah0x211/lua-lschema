3d70fde4eb5bf5d3e56722ce7ee317d271ba9995
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-04 23:28:38 +0900

    add: test/isa_uint8_try.lua has been added.

---
+++ test/isa_uint8_try.lua
@@ -0,0 +1,52 @@
+local errno = require('lschema.ddl.errno');
+local lschema = require('lschema');
+local myschema = lschema.new('myschema');
+local isa = myschema.isa('uint8');
+local err, _;
+
+isa:makeCheck();
+-- invalid
+for _, val in ipairs({
+    -- boolean
+        true, false,
+    -- string
+        'string', '',
+    -- function
+        function()end,
+    -- table
+        {},
+    -- thread
+        coroutine.create(function() end),
+    -- inf
+        -(1/0), 1/0,
+    -- nan
+        0/0,
+    -- float
+        0.1,
+    -- negative float
+        -0.1,
+    -- int8
+        -128,
+    -- int16
+        -32768, 32767,
+    -- int32
+        -2147483648, 2147483647,
+    -- uint8 max + 1
+        256,
+    -- uint16
+        65535,
+    -- uint32
+        4294967295
+}) do
+    _, err = isa( val );
+    ifNil( err );
+    ifNotEqual( err.errno, errno.ETYPE );
+    ifNotEqual( err.etype, 'ETYPE' );
+end
+
+-- valid
+for val = 0, 255 do
+    _, err = isa( val );
+    ifNotNil( err );
+    ifNotEqual( _, val );
+end
