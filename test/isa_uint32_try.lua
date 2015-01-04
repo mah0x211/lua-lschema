3788ab32cdf26927a104ef731e6dd645595e88f9
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-04 23:32:42 +0900

    add: test/isa_uint32_try.lua has been added.

---
+++ test/isa_uint32_try.lua
@@ -0,0 +1,48 @@
+local errno = require('lschema.ddl.errno');
+local lschema = require('lschema');
+local myschema = lschema.new('myschema');
+local isa = myschema.isa('uint32');
+local err, _;
+
+isa:makeCheck();
+-- invalid
+for _, val in ipairs({
+    -- boolean
+        true, false,
+    -- string
+        'string', '',
+    -- function
+        function()end,
+    -- table
+        {},
+    -- thread
+        coroutine.create(function() end),
+    -- inf
+        -(1/0), 1/0,
+    -- nan
+        0/0,
+    -- float
+        0.1,
+    -- negative float
+        -0.1,
+    -- int8
+        -128,
+    -- int16
+        -32768,
+    -- int32
+        -2147483648,
+    -- uint32 max + 1
+        4294967296
+}) do
+    _, err = isa( val );
+    ifNil( err );
+    ifNotEqual( err.errno, errno.ETYPE );
+    ifNotEqual( err.etype, 'ETYPE' );
+end
+
+-- valid
+for val = 0, 4294967295 do
+    _, err = isa( val );
+    ifNotNil( err );
+    ifNotEqual( _, val );
+end
