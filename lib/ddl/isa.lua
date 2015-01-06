abef7c67c051bf542cb0fd632841658f6e5b3b91
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-06 12:47:39 +0900

    fix: definition function of default value attribute has been fixed.

--- lib/ddl/isa.lua
+++ lib/ddl/isa.lua
@@ -29,6 +29,7 @@
 local halo = require('halo');
 local inspect = require('util').inspect;
 local typeof = require('util').typeof;
+local lastIndex = require('util.table').lastIndex;
 local AUX = require('lschema.aux');
 local Template = require('lschema.ddl.template');
 local Pattern = require('lschema.ddl.pattern');
@@ -215,6 +216,7 @@ end
 
 --- no duplicate
 function ISA:noDup( ... )
+    -- do not pass arguments
     AUX.abort( #{...} > 0, 'should not pass argument' );
     rawset( AUX.getIndex( self ), 'noDup', true );
     
@@ -227,10 +229,8 @@ function ISA:notNull( ... )
     local index = AUX.getIndex( self );
     
     checkOfAttr( index, self.isa );
-    AUX.abort( 
-        #{...} > 0, 
-        'should not pass argument' 
-    );
+    -- do not pass arguments
+    AUX.abort( #{...} > 0, 'should not pass argument' );
     rawset( index, 'notNull', true );
     
     return self;
@@ -252,11 +252,12 @@ function ISA:min( val )
     local numType = self.isa == 'string' and 'uint' or 
                     self.isa == 'number' and 'finite' or
                     self.isa;
-    
+    -- check type
     AUX.abort( 
         not typeof[numType]( val ), 
         'min %q must be %s number', val, numType
     );
+    -- check max constraint
     AUX.abort( 
         typeof[numType]( self.max ) and val > self.max, 
         'min %d must be less than max: %d', val, self.max
@@ -274,11 +275,12 @@ function ISA:max( val )
     local numType = self.isa == 'string' and 'uint' or 
                     self.isa == 'number' and 'finite' or
                     self.isa;
-    
+    -- check type
     AUX.abort( 
         not typeof[numType]( val ), 
         'max %q must be %s number', val, numType
     );
+    -- check min constraint
     AUX.abort( 
         typeof[numType]( self.min ) and val < self.min, 
         'max %d must be greater than min: %d', val, self.min
@@ -297,6 +299,7 @@ function ISA:pattern( val )
         'pattern must be instance of Pattern'
     );
     
+    -- check default value
     if not typeof.Function( self.default ) then
         AUX.abort(
              not val:exec( self.default ),
@@ -317,28 +320,99 @@ function ISA:default( val )
     local index = AUX.getIndex( self );
     local isa = self.isa;
     local aka = ISA_AKA[isa] or isa;
+    local tail = 1;
+    local arr, len;
     
     checkOfAttr( index, isa );
-    AUX.abort( 
-        not typeof[aka]( val ), 
-        'default value %q must be type of %s', val, aka 
-    );
-    
-    if isa == 'enum' then
+    if self.asArray then
+        arr = val;
         AUX.abort( 
-            not self.enum( val ), 
-            'default value %q is not defined at enum %q',
-            val, self.enum['@'].name
+            not typeof.table( arr ), 
+            'default value %q must be type of array(table)', arr
         );
-    elseif self.isa == 'string' and
-           halo.instanceof( self.pattern, Pattern ) then
-        AUX.abort(
-            not self.pattern:exec( val ),
-            'default value %q does not match pattern %q',
-            val, self.pattern['@'].name
+        
+        -- check last index
+        tail = lastIndex( arr );
+        AUX.abort( 
+            tail < 1,
+            'default value %q index must be start at 1', arr
         );
+        
+        -- check array length constraint
+        if typeof.table( self.len ) then
+            len = self.len;
+            AUX.abort( 
+                tail < len.min, 
+                'default value %q length must be greater than %d', arr, len.min
+            );
+            AUX.abort( 
+                len.max and tail > len.max, 
+                'default value %q length must be less than %d', arr, len.max
+            );
+        end
+    -- wrap to table
+    else
+        arr = { val };
     end
-    rawset( index, 'default', val );
+    
+    -- check value
+    for i = 1, tail do
+        len = nil;
+        val = arr[i];
+        
+        -- check type
+        AUX.abort( 
+            not typeof[aka]( val ), 
+            'default value %q must be type of %s', val, aka 
+        );
+        
+        -- for enum
+        if isa == 'enum' then
+            AUX.abort( 
+                not self.enum( val ), 
+                'default value %q is not defined at enum %q',
+                val, self.enum['@'].name
+            );
+        else
+            -- for string
+            if isa == 'string' then
+                -- check pattern constraint
+                if halo.instanceof( self.pattern, Pattern ) then
+                    AUX.abort(
+                        not self.pattern:exec( val ),
+                        'default value %q does not match pattern %q',
+                        val, self.pattern['@'].name
+                    );
+                end
+                len = #val;
+            -- for number
+            elseif typeof.finite( val ) then
+                len = val;
+            end
+            
+            -- check min/max constraint
+            if len then
+                -- min check
+                if typeof.finite( self.min ) then
+                    AUX.abort(
+                        len < self.min,
+                        'default value %q must be greater than %d',
+                        val, self.min
+                    );
+                end
+                -- max check
+                if typeof.finite( self.max ) then
+                    AUX.abort(
+                        len > self.max,
+                        'default value %q must be less than %d',
+                        val, self.max
+                    );
+                end
+            end
+        end
+    end
+    
+    rawset( index, 'default', self.asArray and arr or arr[1] );
     
     return self;
 end
